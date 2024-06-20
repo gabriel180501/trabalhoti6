0d4eb9b45338e99f07b6c5455ebaeb5c21cff545
Marco Aurélio De Noronha Santos <1246670@sga.pucminas.br>
2024-06-19 21:51:30 -0300

    adding frontend

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import webbrowser
-from flask import Flask, request, jsonify, send_from_directory
+import requests
+from flask import Flask, request, jsonify, send_from_directory, render_template_string
 from werkzeug.utils import secure_filename
 from threading import Timer
 from flask_cors import CORS
@@ -10,6 +11,9 @@ CORS(app)  # Enable CORS to allow requests from all origins
 
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# URL do backend para classificação
+BACKEND_URL = 'http://localhost:5001/predict'  # Alterar para o URL correto do seu backend
 
 @app.route('/predict', methods=['POST'])
 def upload_file():
@@ -26,13 +30,23 @@ def upload_file():
             filename = secure_filename(file.filename)
             file_path = os.path.join(UPLOAD_FOLDER, filename)
             file.save(file_path)
-            return jsonify({'message': 'File successfully uploaded'}), 200
+
+            # Enviar a imagem para o backend
+            with open(file_path, 'rb') as f:
+                response = requests.post(BACKEND_URL, files={'file': f})
+            
+            # Verificar se a resposta é bem-sucedida
+            if response.status_code == 200:
+                return jsonify(response.json()), 200
+            else:
+                return jsonify({'error': 'Failed to get prediction from backend'}), response.status_code
+
     except Exception as e:
         return jsonify({'error': f'Error during file upload: {str(e)}'}), 500
 
 @app.route('/')
 def index():
-    return send_from_directory('static', 'principal.html')
+    return send_from_directory('static', 'index.html')
 
 @app.route('/<path:filename>')
 def serve_static(filename):
